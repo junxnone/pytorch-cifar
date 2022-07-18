16f5bd5831e9dfb2d1ca9f5076f87064c86c28af
junxnone <minx.li@intel.com>
2022-07-18 15:12:16 +0800

    images dir dataset
    Signed-off-by: junxnone <minx.li@intel.com>

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--train_data_path', '-trd', default='data/train', type=str, help='training data path')
+parser.add_argument('--test_data_path', '-ted', default='data/test', type=str, help='test data path')
+parser.add_argument('--epochs', '-e', default=10, type=int, help='training epochs')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
@@ -39,13 +42,17 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
+#trainset = torchvision.datasets.CIFAR10(
+#    root='./data', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.ImageFolder(args.train_data_path, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
+#testset = torchvision.datasets.CIFAR10(
+#    root='./data', train=False, download=True, transform=transform_test)
+
+testset = torchvision.datasets.ImageFolder(args.test_data_path, transform=transform_train)
+
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
@@ -61,14 +68,14 @@ print('==> Building model..')
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
 # net = MobileNet()
-# net = MobileNetV2()
+net = MobileNetV2()
 # net = DPN92()
 # net = ShuffleNetG2()
 # net = SENet18()
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
 # net = RegNetX_200MF()
-net = SimpleDLA()
+# net = SimpleDLA()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -86,12 +93,11 @@ if args.resume:
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr,
                       momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
+#scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
 
 # Training
 def train(epoch):
-    print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
@@ -148,7 +154,8 @@ def test(epoch):
         best_acc = acc
 
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+args.epochs):
+    print(f'\nEpoch/Total Epochs: {epoch}/{args.epochs}')
     train(epoch)
     test(epoch)
     scheduler.step()
