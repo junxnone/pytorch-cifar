9d0d23a7e613ec4dbdb40555ef50d955374fbb11
junxnone <junchen0102@gmail.com>
2022-07-22 15:07:07 +0800

    fix
    Signed-off-by: junxnone <junchen0102@gmail.com>

--- predict_web.py
+++ predict_web.py
@@ -9,16 +9,11 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
         'dog', 'frog', 'horse', 'ship', 'truck')
-model_file = '/cnvrg/checkpoint/ckpt.pth'
+model_file = 'output/ckpt.pth'
 model  = torch.load(model_file, map_location='cpu')
 model = model.to(device)
 model.eval()
-def install_req():
-    command = [
-        'pip3 install torch torchvision',
-    ]
-    subprocess.call(command)
-install_req()
+
 def predict_image(file_path):
     image = torch.tensor(np.array(Image.open(file_path)))
     image = torch.reshape(image,(1,3,32,32)).float()
--- training.sh
+++ training.sh
@@ -1,3 +1,2 @@
-pip3 install torch torchvision
 mkdir -p output
 python3 train.py -trd /data/cifar10c/train -ted /data/cifar10c/test -e 1
