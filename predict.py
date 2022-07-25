71da433597e340c68a4b45de610711a25115de1e
JunX <junchen0102@gmail.com>
2022-07-25 10:58:06 +0800

    Update and rename predict_web.py to predict.py

--- predict.py
+++ predict.py
@@ -16,12 +16,12 @@ model  = torch.load(model_file, map_location='cpu')
 model = model.to(device)
 model.eval()
 
-def predict_image(file_path):
+def predict(file_path):
     image = torch.tensor(np.array(Image.open(file_path)))
     image = torch.reshape(image,(1,3,32,32)).float()
     input = image.to(device)
     output = model(input)
     _, pred = output.topk(5, 1, largest=True, sorted=True)
     result = f'predict: {classes[pred[0][0]]}'
-    e.log_metric("class", classes[pred[0][0]])
+    e.log_metric("class", pred[0][0])
     return result
