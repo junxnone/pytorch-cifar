f3254980349c7dd960b602983e81a989226bfc98
JunX <junchen0102@gmail.com>
2022-07-27 15:14:07 +0800

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,3 +1,3 @@
 mkdir -p output
-python3 train.py -trd /data/cifar10c/train -ted /data/cifar10c/test -e 10
+python3 train.py -trd /data/cifar10c/train -ted /data/cifar10c/test -e 1
 python3 export_ir.py
